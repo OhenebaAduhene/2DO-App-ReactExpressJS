bdda5d5d156baa68de4b2faa5ef1dc9ae19f9552
OhenebaAduhene <aduhene97@outlook.com>
2022-12-28 23:54:36 -0600

    connect server app with mongo db

--- server/index.js
+++ server/index.js
@@ -8,9 +8,16 @@ const app = express()
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+dotenv.config()
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.DB_URL).then(() => console.log("DB conneced")).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     console.log('hello world')
 })
+
+
+
 
 app.listen(3000)
